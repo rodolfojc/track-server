c597c565c1e69696da670a6c577196c4bc0c4d7e
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-22 16:21:11 +0000

    The Signin Route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -14,8 +14,30 @@ router.post('/signup', async (req, res) => {
 
     const token = jwt.sign({ userId: user._id}, "MY_SECRET_KEY");
     res.send({ token });
-    }catch (err) {
+    } catch (err) {
         return res.status(422).send(err.message);
+    }
+});
+
+router.post('/sigin', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password){
+        return res.status(422).send({ error: 'Must provide email and password' });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).send({ error: 'Invalid password or email' });
+    }
+
+    try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
+
+    } catch (err) {
+        return res.status(42).send({ error: 'Invalid password or email' })
     }
 });
 
